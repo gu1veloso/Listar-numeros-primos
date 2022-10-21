34cf2125046286f1402a45f72771904da7d7729f
gu1veloso <guilhermevelosos@gmail.com>
2022-10-20 21:52:45 -0300

    Update numerosPrimos.py
    Mudança nas variáveis

--- numerosPrimos.py
+++ numerosPrimos.py
@@ -1,15 +1,14 @@
 mult = 0
 lista = []
 
-for n in range(0, 100):
+for n in range(2, 100):
 
-    for count in range(2, n):
-        if (n % count == 0):
+    for div in range(2, n-1):
+        if (n % div == 0):
             mult += 1
 
     if (mult == 0):
         lista.append(n)
+        print(lista)
     else:
         mult = 0
-
-print(lista)
