edc0b3f4ec849526a13403f4a4acc1901ccf1404
gu1veloso <guilhermevelosos@gmail.com>
2022-11-16 17:12:13 -0300

    Inseri comentários e modifiquei a lista

--- numerosPrimos.py
+++ numerosPrimos.py
@@ -4,20 +4,21 @@ tempo_inicial = time.time()
 mult = 0
 lista = []
 
-for n in range(2, 100):
+for n in range(2, 100):         #Percorre os números de 2 a 100
 
-    for div in range(2, n-1):
-        if (n % div == 0):
-            mult += 1
+    for div in range(2, n-1):   #Percorre os divisores de 2 ao número a se verificar
+        if (n % div == 0):      #Verifica se a divisão resulta em resto zero
+            mult += 1           #Soma um à variável mult
 
-    if (mult == 0):
-        lista.append(n)
-        print(lista)
-    else:
-        mult = 0
+    if (mult == 0):             #Verifica se a variável mult é nula
+        lista.append(n)         #Acrescenta o número n à lista de números primos
+    else:               
+        mult = 0                #Zera a variável mult
+
+print(lista)                    #Imprime a lista de números primos de 1 a 100
 
 time.sleep(1)
 tempo_final = time.time()
-print(f"{tempo_final - tempo_inicial} segundos")
+print(f"O programa demorou {tempo_final - tempo_inicial} segundos para executar")
 
 
